95989051ce958fd6143a42935b6f7b85e5e9ce19
monicamarcus <monicamarcus@Monicas-MacBook.local>
2016-02-14 19:50:17 -0800

    Added next track functionality and some unit test

--- app/src/main/java/com/example/monicamarcus/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/example/monicamarcus/mymusicplayer/MainActivity.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -16,6 +18,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,18 +33,22 @@ import org.json.JSONObject;
 import static junit.framework.Assert.assertEquals;
 
 public class MainActivity extends Activity {
-    private static String endpoint = "http://streaming.earbits.com/api/v1/track.json?stream_id=5654d7c3c5aa6e00030021aa";
+    private static final String endpoint = "http://streaming.earbits.com/api/v1/track.json?stream_id=5654d7c3c5aa6e00030021aa";
 
     private static final String TAG_ARTIST_NAME = "artist_name";
     private static final String TAG_TRACK_NAME = "name";
     private static final String TAG_COVER_IMAGE_URL = "cover_image";
     private static final String TAG_TRACK_URL = "media_file";
 
-    private static String track_url = null;
+    private static ArrayList<Song> songList = new ArrayList<>();
+    private static Song currentSong = null;
+    private static int currentPosition = -1;
 
     TextView tvResponse;
     TextView tvIsConnected;
     ImageView coverImage;
+    Button nextTrackBtn;
+    Button previousTrackBtn;
     ProgressDialog mProgressDialog;
     MediaPlayer mp;
 
@@ -53,6 +60,10 @@ public class MainActivity extends Activity {
         tvResponse = (TextView) findViewById(R.id.tvResponse);
         tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
         coverImage = (ImageView) findViewById(R.id.image);
+        previousTrackBtn = (Button) findViewById(R.id.previousTrackBt);
+        previousTrackBtn.setOnClickListener(handlerPreviousTrack);
+        nextTrackBtn = (Button) findViewById(R.id.nextTrackBt);
+        nextTrackBtn.setOnClickListener(handlerNextTrack);
 
         assertEquals("text should be empty", "", tvResponse.getText().toString());
 
@@ -66,16 +77,54 @@ public class MainActivity extends Activity {
         }
     }
 
-    //When the Next Track button is clicked
-    public void nextTrack(View view) {
-        new HttpAsyncTask().execute(endpoint);
-    }
+    View.OnClickListener handlerNextTrack = new View.OnClickListener() {
+        public void onClick(View v) {
+            if (mp != null) mp.stop();
+            if ((!songList.isEmpty()) && currentPosition < songList.size() - 1) {
+                currentPosition++;
+                currentSong = songList.get(currentPosition);
+                tvResponse.setText(currentSong.toString());
+                new getImageAsyncTask().execute(currentSong.getImageURL());
+                play(currentSong.getTrackURL());
+            } else
+                new HttpAsyncTask().execute(endpoint);
+            if (currentSong != null && !songList.contains(currentSong)) {
+                songList.add(currentSong);
+            }
+            if (currentSong != null)
+                currentPosition = songList.indexOf(currentSong);
+        }
+    };
 
-    //When the Play button is clicked
-    public void playNextTrack(View view) {
+    View.OnClickListener handlerPreviousTrack = new View.OnClickListener() {
+        public void onClick(View v) {
+            if (mp != null) mp.stop();
+            songList.trimToSize();
+            if (!songList.isEmpty()) {
+                currentPosition = songList.indexOf(currentSong) - 1;
+                if (currentPosition >= 0) {
+                    currentSong = songList.get(currentPosition);
+                    tvResponse.setText(currentSong.toString());
+                    new getImageAsyncTask().execute(currentSong.getImageURL());
+                    play(currentSong.getTrackURL());
+                } else {
+                    tvResponse.setText("There is no previous song in the list");
+                    coverImage.setImageBitmap(null);
+                }
+            } else {
+                currentSong = null;
+                currentPosition = -1;
+                tvResponse.setText("Get a new song, the list is empty.");
+                coverImage.setImageBitmap(null);
+            }
+        }
+    };
+
+    //play the track from external URL
+    public void play(String trackURL) {
         try {
             mp = new MediaPlayer();
-            mp.setDataSource(track_url);
+            mp.setDataSource(trackURL);
             mp.prepare();
             mp.start();
         } catch (java.io.IOException e) {
@@ -87,40 +136,38 @@ public class MainActivity extends Activity {
     public void pause(View view) {
         if (mp != null)
             mp.pause();
-        else Log.e("MainActivity", "pause: mp is null");
     }
 
     //When the Resume button is clicked
     public void resume(View view) {
-        mp.start();
+        if (mp != null)
+            mp.start();
     }
 
     //When the Stop button is clicked
     public void stop(View view) {
-        mp.stop();
-        mp.release();
+        if (mp != null) {
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
     }
 
     public static String GET(String url){
         InputStream inputStream = null;
         String result = "";
         try {
-
             // create HttpClient
             HttpClient httpclient = new DefaultHttpClient();
-
             // make GET request to the given URL
             HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
-
             // receive response as inputStream
             inputStream = httpResponse.getEntity().getContent();
-
             // convert inputstream to string
             if(inputStream != null)
                 result = convertInputStreamToString(inputStream);
             else
                 result = "Did not work!";
-
         } catch (Exception e) {
             Log.d("InputStream", e.getLocalizedMessage());
         }
@@ -142,10 +189,7 @@ public class MainActivity extends Activity {
     public boolean isConnected(){
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected())
-            return true;
-        else
-            return false;
+        return networkInfo != null && networkInfo.isConnected();
     }
 
     private class getImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
@@ -188,62 +232,60 @@ public class MainActivity extends Activity {
         }
     }
 
-
     private class HttpAsyncTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
-
             return GET(urls[0]);
         }
 
         @Override
         protected void onPostExecute(String result) {
             Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_SHORT).show();
-            Log.v("RESULT",result);
             try {
                 JSONObject json = new JSONObject(result);
-                String text = "TRACK NAME: " + getTrackName(json) + "\n" + "ARTIST NAME: " + getArtistName(json);
-                tvResponse.setText(text);
+                currentSong = new Song(getTrackURL(json), getTrackName(json), getArtistName(json), getCoverImageURL(json));
+                tvResponse.setText(currentSong.toString());
                 assertEquals("text should contain", "TRACK NAME: ", tvResponse.getText().toString().substring(0, 12));
-                new getImageAsyncTask().execute(getCoverImageURL(json));
-                track_url = getTrackURL(json);
+                String imageURL = getCoverImageURL(json);
+                new getImageAsyncTask().execute(imageURL);
+                String trackURL = getTrackURL(json);
+                currentSong.setImageURL(imageURL);
+                songList.add(currentSong);
+                play(trackURL);
             } catch (JSONException e) {
                 e.printStackTrace();
-                //e.getMessage();
             }
         }
     }
 
     private String getTrackName(JSONObject json) {
-        String s = null;
+        String trackName = "";
         try {
-            s = json.getString(TAG_TRACK_NAME);
+            trackName = json.getString(TAG_TRACK_NAME);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         finally {
-            return s;
+            return trackName;
         }
     }
 
     private String getArtistName(JSONObject json) {
-        String s = null;
+        String artistName = "";
         try {
-            s = json.getString(TAG_ARTIST_NAME);
+            artistName = json.getString(TAG_ARTIST_NAME);
         } catch (JSONException e) {
-            Log.v("EXCEPTION", e.getMessage());
             e.printStackTrace();
         }
         finally {
-            return s;
+            return artistName;
         }
     }
 
     private String getCoverImageURL(JSONObject json){
-        String imageURL = null;
+        String imageURL = "";
         try {
             imageURL = json.getString(TAG_COVER_IMAGE_URL);
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -253,7 +295,7 @@ public class MainActivity extends Activity {
     }
 
     private String getTrackURL(JSONObject json) {
-        String trackURL = null;
+        String trackURL = "";
         try {
             trackURL = json.getString(TAG_TRACK_URL);
         }  catch (JSONException e) {
--- app/src/test/java/com/example/monicamarcus/mymusicplayer/ExampleUnitTest.java
+++ app/src/test/java/com/example/monicamarcus/mymusicplayer/ExampleUnitTest.java
@@ -1,15 +1,107 @@
 package com.example.monicamarcus.mymusicplayer;
 
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.app.Activity;
+import android.widget.TextView;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+
+    private int currentPosition;
+    private ArrayList<Song> songList;
+
+    Activity myActivity = new MainActivity();
+    Song mySong = new Song();
+
+    public boolean isCurrentPositionCorrect() {
+        return (songList.size() <= 0) || currentPosition < songList.size();
     }
+
+    @Before
+    public void setUp() {
+        songList = new ArrayList<>();
+        currentPosition = -1;
+    }
+
+    @Test
+    public void testCurrentPosition() throws Exception {
+        boolean p = isCurrentPositionCorrect();
+        if (BuildConfig.DEBUG && !p) {
+            throw new AssertionError();
+        }
+        assertTrue(currentPosition < songList.size());
+    }
+
+    @UiThreadTest
+    public void testNextTrackButton() throws Exception {
+        Button mButton = (Button) myActivity.findViewById(R.id.nextTrackBt);
+        songList.add(mySong);
+        mButton.performClick();
+        mButton.performClick();
+        boolean p = isCurrentPositionCorrect();
+        if (BuildConfig.DEBUG && !p) {
+            throw new AssertionError();
+        }
+        songList.trimToSize();
+        assertTrue(currentPosition < songList.size());
+    }
+
+    @UiThreadTest
+    public void testPreviousTrackButton_1() throws Exception {
+        Button mButton = (Button) myActivity.findViewById(R.id.previousTrackBt);
+        songList.add(mySong);
+        mButton.performClick();
+        mButton.performClick();
+        boolean p = isCurrentPositionCorrect();
+        if (BuildConfig.DEBUG && !p) {
+            throw new AssertionError();
+        }
+        songList.trimToSize();
+        assertTrue(currentPosition < songList.size());
+    }
+
+    @UiThreadTest
+    public void testPreviousTrackButton_2() {
+        TextView tvResponse = (TextView) myActivity.findViewById(R.id.tvResponse);
+        Button mButton = (Button) myActivity.findViewById(R.id.previousTrackBt);
+        mButton.performClick();
+        assertEquals(tvResponse.getText().toString(), "a");
+    }
+
 }
+
+
+
+/*
+public class ApplicationTest extends MyMusicPlayerTestCase2<MainActivity> {
+    public ApplicationTest() {
+        super(MainActivity.class);
+    }
+
+    public void testListPrevious() {
+        Activity activity = getActivity();
+        //Button btnNext = (Button) activity.findViewById(R.id.nextTrack);
+        Button btnPrev = (Button) activity.findViewById(R.id.previousTrack);
+
+        int listSize = MainActivity.songList.size();
+        int index = MainActivity.currentPosition;
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //nextTrack(btnNext);
+                previousTrack(btnPrev);
+                assertEquals(listSize > 1 && index == 1,index == 0);
+            }
+        });
+        activity.finish();
+
+    }
+}*/
